6f5e9a002b4e1ce84ef97f728fc5cac72904120f
eliyajoseph7 <exaveryeliya20@gmail.com>
2019-12-28 18:53:32 +0300

    function for pulling payments details in customer model

--- app/Customer.php
+++ app/Customer.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App;
-
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'street', 'gender', 'phone', 'metre_no',
+    ];
+
+
+
+    public function payments(){
+
+        return $this->hasOne('App\Payment');
+    }
 }
