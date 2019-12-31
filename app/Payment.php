42823ed477f522d3005c277f97a1e3f6a9625566
eliyajoseph7 <exaveryeliya20@gmail.com>
2019-12-31 19:01:36 +0300

    creating customer function

--- app/Payment.php
+++ app/Payment.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     //
+
+    public function customer(){
+
+        // return $this->hasOne('App\Payment');
+        return $this->belongsTo(Customer::class);
+    }
 }
