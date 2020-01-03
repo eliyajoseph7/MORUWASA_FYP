d3db050b26f04b4a19f3af573bce946612714f6e
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-01-03 20:48:27 +0300

    displaying total registered customers

--- resources/views/billshome.blade.php
+++ resources/views/billshome.blade.php
@@ -34,7 +34,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total customers</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">1785</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">{{( $count )}}</div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-users fa-2x text-gray-300"></i>
