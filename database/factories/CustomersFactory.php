8ea6a69bfd8d46b2c11844f923a6ed805bc22cde
eliyajoseph7 <eliyajoseph7@gmail.com>
2020-01-13 18:40:15 +0300

    creating and modifying CustomersFactory

---
+++ database/factories/CustomersFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Customer;
+use Faker\Generator as Faker;
+
+$factory->define(Customer::class, function (Faker $faker) {
+    return [
+        //
+        'name' => $faker->name,
+        'street' => $faker->randomElement(['Bigwa', 'Boma', 'Msanvu', 'Kihonda', 'Mazimbu', 'Mbuyuni', 'Mindu', 'Sabasaba', 'Mzinga']),
+        'gender' => $faker->randomElement(['M', 'F']),
+        'phone' => $faker->phoneNumber,
+        
+
+        
+    ];
+});
