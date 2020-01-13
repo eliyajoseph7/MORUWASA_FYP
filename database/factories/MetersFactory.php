e794edce5ca8cd89ea130b8c38eda3a4fee14ee0
eliyajoseph7 <eliyajoseph7@gmail.com>
2020-01-13 18:40:58 +0300

    creating and modifying MetersFactory

---
+++ database/factories/MetersFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Meter;
+use Faker\Generator as Faker;
+
+$factory->define(Meter::class, function (Faker $faker) {
+    return [
+        //
+               
+        'meter_no' => $faker->unique()->numberBetween($min = 1, $max = 50000000),
+        'customer_id' => $faker->unique()->numberBetween($min = 1, $max = 100),
+
+        
+        // 'customer_id' => 
+        // function () {
+        //     return factory(App\Customer::class)->create()->id;
+        // }
+
+
+    ];
+});
