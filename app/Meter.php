6d9f6dead5cba14c53d87f338ea646d3b04221ae
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-03-03 23:00:18 +0300

    fillable modified

--- app/Meter.php
+++ app/Meter.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Meter extends Model
 {
     //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'customer_id', 'type', 'meter_no',
+    ];
+
     public function customer(){
 
         return $this->belongsTo(Customer::class);
