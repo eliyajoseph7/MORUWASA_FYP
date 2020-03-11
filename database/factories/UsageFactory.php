8233ba25d721432de7f9bd94d332db056a478c4d
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-03-11 12:05:41 +0300

    unrequired column deleted

--- database/factories/UsageFactory.php
+++ database/factories/UsageFactory.php
@@ -8,7 +8,8 @@ use Faker\Generator as Faker;
 $factory->define(Usage::class, function (Faker $faker) {
     return [
         //
-        'customer_id' => $faker -> numberBetween($min = 1, $max = 20)
+        'customer_id' => $faker -> numberBetween($min = 1, $max = 1000),
+
 
     ];
 });
