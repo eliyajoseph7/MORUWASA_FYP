4939aefc281eb6973f1cb69c2a4de1cba2171d94
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-03-11 11:33:37 +0300

    joining usages and consuptions tables

--- app/Http/Controllers/WaterBillController.php
+++ app/Http/Controllers/WaterBillController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Input;
 use Illuminate\Http\Request;
 use App\WaterBill;
 use App\Consuption;
@@ -11,18 +11,30 @@ use App\Usage;
 class WaterBillController extends Controller
 {
     //
-    public function index(){
-        $bill = WaterBill::all();
-        // $usage = Usage::all()->groupBy('customer_id');
-        // return $usage;
-        $result = DB::table('usages')
-                        ->join('consuptions', 'usages.id', '=', 'consuptions.usage_id')
-                        ->select('usages.id', 'customer_id', 'consuptions.consuption')
+    public function index(Request $request){
+        $bill = new WaterBill;
+        // $usages = Usage::select('id', 'customer_id', 'consuption')
+        //                 ->groupBy('customer_id')
+        //                 ->sum('consuption');
+
+        // $usages = Usage::select('customer_id', DB::raw("SUM(consuption) as units"))
+        //                 ->groupBy(DB::raw("customer_id"))
+        //                 ->get();
+                        
+                // print_r($usages);
+        // return $usages; 
+        $usages = Usage::join('consuptions', 'usages.id', '=', 'consuptions.id')
+                        ->select('customer_id', DB::raw("SUM(consuptions.consuption) as units"))
+                        ->groupBy('customer_id')
                         ->get();
 
-        // $data = $result -> customer_id;
-        return $result;    
+        // foreach ($usages->all() as $usage) {
+            // $bill -> customer_id = $usages->all() -> customer_id;
+            // $bill -> units = $usages->all() -> units;
+            // $bill -> save();
+        // }
+        // return $usages;    
 
-        return view('waterBill', ['bill' => $bill, 'result' => $result]);
+        return view('waterBill', ['bill' => $bill, 'usages' => $usages]);
     }
 }
