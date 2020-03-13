bf30b0a2e94281e87b1ea5a001e2ccf9000b9d96
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-03-13 20:48:52 +0300

    codes modification for datareceived from water meter

--- app/Http/Controllers/Api/v1/ConsuptionController.php
+++ app/Http/Controllers/Api/v1/ConsuptionController.php
@@ -1,22 +1,51 @@
 <?php
 
 namespace App\Http\Controllers\Api\v1;
-
+use Illuminate\Support\Facades\Input;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Consuption;
 use App\Http\Resources\ConsuptionResource;
+use App\Meter;
+use App\Usage;
 
 class ConsuptionController extends Controller
 {
     //
     public function store(Request $request): ConsuptionResource
     {
-        $request -> validate([
-            'consuption' => 'required'
+        $request -> validate([  //validating the inputs received from real time meter
+            'consuption' => 'required',
+            'meter_no' => 'required'
         ]);
+        $meter = new Meter;
+        if ($meter::where('meter_no', '=', Input::get('meter_no'))->exists()) // checking if the meter number match with any available in the maters table
+        {
+            $customer = $meter::where('meter_no', Input::get('meter_no'))->pluck('customer_id');// taking a customer id of the matched meter
+            foreach($customer as $customer){
+                $customer = $customer;
+                
+            }
 
-        $units = Consuption::create($request->all());
-        return new ConsuptionResource($units);
+            if(is_null($customer))
+            {
+                return new ConsuptionResource(["Oops!, the meter number is not yet assigned to any customer"]);
+            }else{
+                        
+                $usage = new Usage;
+                $usage -> customer_id = $customer;
+                $usage->save();
+
+                $units = Consuption::create($request->all());
+                return new ConsuptionResource($units);
+            }
+    
+
+        }else{
+            return new ConsuptionResource(["The meter number didn't match with any customer's meter"]);
+
+        }
+
+        
     }
 }
