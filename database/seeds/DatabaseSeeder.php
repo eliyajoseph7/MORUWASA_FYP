85a914e35a47b42be6c5b6326336a6dbd20046b9
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-03-23 23:44:04 +0300

    test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,23 +12,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        factory(App\Customer::class, 500)->create()->each(function($customers){
-            $customers->save();
-            });
+        // factory(App\Customer::class, 500)->create()->each(function($customers){
+        //     $customers->save();
+        //     });
 
-        factory(App\Meter::class, 500)->create()->each(function($meters){
-            $meters->save();
-            });
+        // factory(App\Meter::class, 500)->create()->each(function($meters){
+        //     $meters->save();
+        //     });
 
-        factory(App\Payment::class, 500)->create()->each(function($payments){
-            $payments->save();
-            });
+        // factory(App\Payment::class, 500)->create()->each(function($payments){
+        //     $payments->save();
+        //     });
 
-        factory(App\Usage::class, 2000)->create()->each(function($usage){
-            $usage->save();
-            });
+        // factory(App\Usage::class, 2000)->create()->each(function($usage){
+        //     $usage->save();
+        //     });
 
-        factory(App\Consuption::class, 300)->create()->each(function($consuption){
+        factory(App\Consuption::class, 700)->create()->each(function($consuption){
             $consuption->save();
             });
     }
