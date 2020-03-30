634e42d755565e36a5c4000f1925855f030a7a87
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-03-30 14:03:03 +0300

    fetching meters with no customers

--- app/Http/Controllers/actions/ActionToCustomerController.php
+++ app/Http/Controllers/actions/ActionToCustomerController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Customer;
 use App\Usage;
+use App\Meter;
 
 class ActionToCustomerController extends Controller
 {
@@ -22,12 +23,14 @@ class ActionToCustomerController extends Controller
         $view = Customer::find($id);
         $daily_consuption = Usage::join('consuptions', 'usages.id', '=', 'consuptions.id')
                                     ->where('usages.customer_id', $id)
-                                    ->whereYear('consuptions.created_at', date('Y'))
                                     // ->whereMonth('consuptions.created_at', date('m'))
                                     ->select('consuptions.created_at',DB::raw('SUM(cast(consuptions.consuption as double precision))'))
                                     ->groupBy('consuptions.created_at')
                                     ->get();
-        // return $daily_consuption;                            
-        return view('actionToCustomers/view', compact('view','daily_consuption'));
+        // return $daily_consuption;  
+
+        $meter = Meter::where('customer_id',null)
+                        ->get();
+        return view('actionToCustomers/view', compact('view','daily_consuption','meter'));
     }
 }
