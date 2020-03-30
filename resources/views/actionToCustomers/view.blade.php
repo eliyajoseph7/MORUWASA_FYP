d85430ffd2b482e9504c6c5c6cdfbc24dc8acce5
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-03-30 23:27:53 +0300

    text changed

--- resources/views/actionToCustomers/view.blade.php
+++ resources/views/actionToCustomers/view.blade.php
@@ -328,11 +328,11 @@
           </div>
         </div>
 
-        <!-- Side Widget -->
+        <!-- Payment Status -->
         <div class="card my-4">
-          <h5 class="card-header">Side Widget</h5>
+          <h5 class="card-header">Payment Status</h5>
           <div class="card-body">
-            You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+            This part is not yet implemented!
           </div>
         </div>
 
