47559cd62278c9dc12fd10e28e211d05a5751d81
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-04-01 22:52:28 +0300

    modification on profile image and dropdown

--- resources/views/constants/headerAndSide.blade.php
+++ resources/views/constants/headerAndSide.blade.php
@@ -161,10 +161,22 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                <span><img src="{{ url('uploads/staff/'. Auth::user()->image ) }}" class="avatar"></span>{{ Auth::user()->username }}  <span class="caret"></span> 
+                                <span><img src="{{ url('uploads/staff/'.Auth::user()->image ) }}" class="avatar" style="width: 30px; border-radius: 50%; height:30px;"></span> {{ Auth::user()->username }}<span class="caret"></span> 
                                  </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <!-- <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div> -->
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href='{{ url("/profile/".Auth::user()->id) }}'>Edit Profile</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
