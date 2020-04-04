ecacfa1f96c07357a20e7daa18a0e692c0495ee8
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-04-04 22:43:58 +0300

    testing

--- config/nexmo.php
+++ config/nexmo.php
@@ -12,8 +12,8 @@ return [
     |
     */
 
-    'api_key'    => function_exists('env') ? env('NEXMO_KEY', '') : '',
-    'api_secret' => function_exists('env') ? env('NEXMO_SECRET', '') : '',
+    'api_key'    => function_exists('env') ? env('NEXMO_KEY', '16889aad') : '',
+    'api_secret' => function_exists('env') ? env('NEXMO_SECRET', 'm63EKAxdng3KjYzQ') : '',
 
     /*
     |--------------------------------------------------------------------------
