eacc3f47938786f60c2a0bae504b893350a9a382
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-04-15 00:14:54 +0300

    changes for rendering a dynamic chart

--- resources/views/billshome.blade.php
+++ resources/views/billshome.blade.php
@@ -98,7 +98,7 @@
                 <!-- Card Body -->
                 <div class="card-body">
                   <div class="chart-area"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
-                  {!! $chart->render() !!}
+                  <canvas id="myAreaChart" width="100%" height="50%" class="chartjs-render-monitor responsive" style="display: block; width: 133px; height: 160px;"></canvas>                  
                   </div>
                 </div>
               </div>
@@ -211,6 +211,7 @@
 </body>
 </html>
 
+<script src="{{url('js/plugins/chartJs/create-charts.js')}}"></script>
 
 <script>
         $(document).ready(function() {
