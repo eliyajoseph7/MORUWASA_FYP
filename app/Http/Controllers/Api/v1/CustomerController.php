bc5316587e8e2073158a1de254c17945f3940ffb
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-05-27 16:37:22 +0300

    api for user login created

--- app/Http/Controllers/Api/v1/CustomerController.php
+++ app/Http/Controllers/Api/v1/CustomerController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api\v1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
 use App\Http\Resources\CustomerResource;
 use App\Customer;
+use App\Meter;
 
 class CustomerController extends Controller
 {
@@ -14,5 +16,26 @@ class CustomerController extends Controller
     {
         $customer -> update($request -> all());
             return new CustomerResource($customer);
+    }
+
+    public function store(Request $request): CustomerResource
+    {
+        $request -> validate([
+            'phone'=> 'required',
+            'meter_no'=> 'required'
+        ]);
+
+        $phone = $request->input('phone');
+        $meter = $request->input('meter_no');
+
+        if(Customer::where('phone', $phone)->exists())
+        {
+            if(Meter::where('meter_no', $meter)->exists())
+            {
+                return new CustomerResource(["Login successfully"]);
+            }
+        }else{
+            return new CustomerResource(["Either phone or meter number was incorrect"]);
+        }
     }
 }
