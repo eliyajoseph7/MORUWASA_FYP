614c203bc603ccc3f7190988d0072ede48f5638e
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-06-14 23:37:46 +0300

    initial commit

---
+++ app/Observers/CustomerObserver.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Observers;
+
+use App\Customer;
+
+class CustomerObserver
+{
+    /**
+     * Handle the Customer "created" event.
+     *
+     * @param  \App\Customer  $customer
+     * @return void
+     */
+    public function creating(Customer $customer)
+    {
+        $customer->api_token = bin2hex(openssl_random_pseudo_bytes(30));
+    }
+
+    public function created(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Handle the Customer "updated" event.
+     *
+     * @param  \App\Customer  $customer
+     * @return void
+     */
+    public function updated(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Handle the Customer "deleted" event.
+     *
+     * @param  \App\Customer  $customer
+     * @return void
+     */
+    public function deleted(Customer $customer)
+    {
+        //
+    }
+}
