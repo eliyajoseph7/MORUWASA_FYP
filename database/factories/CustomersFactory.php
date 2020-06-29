f4215065c29b069bd193e16c6fe1f7d8629a6bf0
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-06-29 13:44:26 +0300

    phone number format changed

--- database/factories/CustomersFactory.php
+++ database/factories/CustomersFactory.php
@@ -6,12 +6,16 @@ use App\Customer;
 use Faker\Generator as Faker;
 
 $factory->define(Customer::class, function (Faker $faker) {
+    $result = '';
+    for($i = 0; $i < 9; $i++) {
+        $result .= mt_rand(0, 9);
+    }
     return [
         //
         'name' => $faker->name,
         'street' => $faker->randomElement(['Bigwa', 'Boma', 'Msanvu', 'Kihonda', 'Mazimbu', 'Mbuyuni', 'Mindu', 'Sabasaba', 'Mzinga']),
         'gender' => $faker->randomElement(['M', 'F']),
-        'phone' => $faker->phoneNumber,
+        'phone' => '+255'.$result,
         'category' => $faker->randomElement(['domestic', 'industry', 'institution', 'commercial', 'tank','kiosk']),
         'api_token' => bin2hex(openssl_random_pseudo_bytes(30))
         
