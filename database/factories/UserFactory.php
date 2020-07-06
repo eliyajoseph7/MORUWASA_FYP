9939a0a399ff11ad60b3afe9b29064e69fdaa08c
eliyajoseph7 <exaveryeliya20@gmail.com>
2020-07-06 22:29:55 +0300

    factory modified

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,10 +17,20 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $result = '';
+    for($i = 0; $i < 9; $i++) {
+        $result .= mt_rand(0, 9);
+    }
     return [
-        'name' => $faker->name,
+        'firstname' => $faker->FirstName,
+        'lastname' => $faker->LastName,
+        'username' => $faker->userName,
+        'permission' => $faker->randomElement(['user']),
+        'occupation' => $faker->randomElement(['bill officer']),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'gender' => $faker->randomElement(['M', 'F']),
+        'phone' => '+255'.$result,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
