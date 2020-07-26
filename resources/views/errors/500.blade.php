dc9deeb02d23370d3322063fd9d4d61a3b85dbc9
eliyajoseph7 <42994583+eliyajoseph7@users.noreply.github.com>
2020-07-26 19:46:43 +0300

    error description modified

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -246,8 +246,8 @@ html {
         <div class="error">
             <h1>500</h1>
             <h2>error</h2>
-            <p>Ruh-roh, something just isn't right... Time to paw through your logs and get down and dirty in your
-                stack-trace;)</p>
+            <p>Whoops!!, something just went wrong... We are tracing the source and we will soon fix it,
+                try again later;)</p>
         </div>
         <div class="stack-container">
             <div class="card-container">
