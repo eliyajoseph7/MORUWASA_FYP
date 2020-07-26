603756789af52248c3f8339a9bd5bd6029aafbd5
eliyajoseph7 <42994583+eliyajoseph7@users.noreply.github.com>
2020-07-26 21:46:01 +0300

    script link for drawing a chart added

--- resources/views/constants/headerAndSide.blade.php
+++ resources/views/constants/headerAndSide.blade.php
@@ -414,6 +414,7 @@
     <script src="{{url('js/plugins/chartJs/Chart.min.js')}}"></script>
     <script src="{{url('js/plugins/chartJs/create-charts-complaints.js')}}"></script>
     <script src="{{url('js/plugins/chartJs/create-charts-complaints-locations.js')}}"></script>
+    <script src="{{url('js/plugins/chartJs/create-charts-waterBillTrend.js')}}"></script>
     <!-- <script src="{{url('js/plugins/chartJs/jquery.min.js')}}"></script> -->
 
 
