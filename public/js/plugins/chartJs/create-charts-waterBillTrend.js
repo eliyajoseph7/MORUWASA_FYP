aa992a7a878e0a45c0f3a4f8934368be577ccb6e
eliyajoseph7 <42994583+eliyajoseph7@users.noreply.github.com>
2020-07-27 00:57:10 +0300

    some modifications have been done

--- public/js/plugins/chartJs/create-charts-waterBillTrend.js
+++ public/js/plugins/chartJs/create-charts-waterBillTrend.js
@@ -29,7 +29,7 @@
 
             var ctx = document.getElementById("billAmountChart");
             var myLineChart = new Chart(ctx, {
-                type: 'line',
+                type: 'bar',
                 data: {
                     labels: response.months, // The response got from the ajax request containing all month names in the database
                     datasets: [{
@@ -44,7 +44,7 @@
                         pointHoverBackgroundColor: "rgba(2,117,216,1)",
                         pointHitRadius: 20,
                         pointBorderWidth: 2,
-                        data: response.complaint_count_data // The response got from the ajax request containing data for the completed jobs in the corresponding months
+                        data: response.consumption_count_data // The response got from the ajax request containing data for the completed jobs in the corresponding months
                     }],
                 },
                 options: {
